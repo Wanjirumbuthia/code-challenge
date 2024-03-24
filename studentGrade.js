b7f81cc59820baf0bf143e2805fdd28e2eb48759
mbuthia wanjiru <sheezwagichuru.gmail.com>
2024-03-25 01:54:43 +0300

    code challenge

---
+++ studentGrade.js
@@ -0,0 +1,16 @@
+let grade = marks
+function studentscore (marks){
+    if (marks >= 79){
+    return"A"
+  }else if(marks < 79 && marks >= 60){
+    return"B"
+  }else if( marks <60 && marks >=49){
+    return"C"
+  }else if( marks >49 && marks >=40){
+    return"D"
+  }else if ( marks < 40){
+    return"E"
+  }
+
+}
+
